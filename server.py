e15d29e7f460fcf7f9f8f9ac9fa21a31d491c9ac
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-31 17:44:44 -0600

    Game Connection Complete

--- server.py
+++ server.py
@@ -5,16 +5,30 @@ app.secret_key = "skittles"
 
 @app.route('/')
 def game_home():
-    session['random_num'] = random.randint(1,100)
+    if 'random_num' not in session:
+        session['random_num'] = random.randint(1,100)
     print(session['random_num'])
     return render_template("index.html")
 
 @app.route('/guess', methods=['POST'])
 def guess():
     if int(request.form['guess']) > session['random_num']:
+        session['color'] = "red1"
+        print(session['color'])
+        return redirect('/')
+    elif int(request.form['guess']) < session['random_num']:
+        session['color'] = "red2"
+        print(session['color'])
+        return redirect('/')
+    else:
+        session['color'] = "green"
+        print(session['color'])
         return redirect('/')
         
-
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 
 
