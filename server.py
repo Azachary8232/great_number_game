86442fa0f6184ce12b732dae207bfe1a1dc70c19
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-31 15:20:32 -0600

    Redirect Complete

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect, request
 import random
 app = Flask(__name__)
 app.secret_key = "skittles"
@@ -6,7 +6,16 @@ app.secret_key = "skittles"
 @app.route('/')
 def game_home():
     session['random_num'] = random.randint(1,100)
+    print(session['random_num'])
     return render_template("index.html")
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    if int(request.form['guess']) > session['random_num']:
+        return redirect('/')
+        
+
+
 
 
 
